b36f17a395238c3b8385fe8cd8cc55aad09e7015
rhwang10 <robert.t.hwang@gmail.com>
2021-12-24 02:30:02 -0500

    Filter out messages that are not active

--- app/sql/messages.py
+++ app/sql/messages.py
@@ -6,4 +6,4 @@ def get_messages(db: Session):
     return db.query(Messages).all()
 
 def get_messages_for_target(db: Session, target_user_id: str):
-    return db.query(Messages).filter(Messages.target_user_id == target_user_id).all()
+    return db.query(Messages).filter(Messages.target_user_id == target_user_id).filter(Messages.is_active == True).all()
