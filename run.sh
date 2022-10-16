b650cbd3e15cf5009bf0c2ffe647e98d4b4e2261
robert <robert@watershedclimate.com>
2022-10-16 11:08:09 -0400

    use database iduna

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ export MODULE=${MODULE-app.main:app}
 export HOST=${HOST:-0.0.0.0}
 export PORT=${PORT:-8001}
 export ENV="dev"
+export DATABASE_URL="postgres://localhost/iduna:5432"
 echo $MODULE
 exec uvicorn --host $HOST --port $PORT --reload "$MODULE"
